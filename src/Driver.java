e048352acd9ef3a2e2b11fb3bd7dfd27dbb21cfd
prrtzel <gsyacoubian@gmail.com>
2024-04-26 11:11:06 -0500

    driver done

---
+++ src/Driver.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Driver {
+    public static void main(String[] args) throws IOException {
+        //----------------------------------------------------------------
+        //read in dictionary.txt
+        FileReader fr = new FileReader("src/dictionary.txt");
+        BufferedReader br = new BufferedReader(fr);
+        List<String> lines = new ArrayList<>();
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            lines.add(line);
+        }
+        br.close();
+        String[] dictionary = new String[lines.size()];
+        lines.toArray(dictionary);
+
+
+        //----------------------------------------------------------------
+        //main loop
+
+        int numOfItems = lines.size();
+        int tableSize = 0;
+        int hashCode = 0;
+
+        for (int load = 70; load <= 100; load++) {
+            //find table size
+            tableSize = numOfItems / load;
+            tableSize = findNextPrime(tableSize);
+
+            String[] hashTable = new String[tableSize];
+            OAL oal = new OAL(hashTable);
+
+            for (int i = 0; i < numOfItems; i++) {
+                //determine hashcode
+                hashCode = dictionary[i].hashCode();
+                hashCode = Math.abs(hashCode % tableSize);
+
+                //Linear insert
+                oal.linearInsert(hashCode, dictionary[i]);
+
+                //Quadratic insert
+                oal.quadraticInsert(hashCode, dictionary[i]);
+            }
+        }
+
+
+    }
+    public static int findNextPrime(int value) {
+        return 0;
+    }
+}
---
+++ src/OAL.java
@@ -0,0 +1,16 @@
+/*
+Takes in an initial hashtable with no values and builds it with
+linear and quadratic insert methods
+ */
+public class OAL {
+    String[] hashTable;
+    public OAL(String[] hashTable) {
+        this.hashTable = hashTable;
+    }
+    public void linearInsert(int hashCode, String str) {
+
+    }
+    public void quadraticInsert(int hashCode, String str) {
+
+    }
+}
