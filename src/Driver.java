227c676c0b2bda8be667281538c6709f9588b6ae
prrtzel <gsyacoubian@gmail.com>
2024-04-26 16:40:25 -0500

    quadratic probe infinitely loops and fixed linear probe

--- src/Driver.java
+++ src/Driver.java
@@ -15,6 +15,7 @@ public class Driver {
             lines.add(line);
         }
         br.close();
+        System.out.println("File Read");
         String[] dictionary = new String[lines.size()];
         lines.toArray(dictionary);
 
@@ -25,11 +26,14 @@ public class Driver {
         int numOfItems = lines.size();
         int tableSize = 0;
         int hashCode = 0;
+        int totalCollisions = 0;
+        int itemsWOCollisions = 0;
 
         for (int load = 70; load <= 100; load++) {
+            System.out.println("Load: " + load);
             //find table size
-            tableSize = numOfItems / load;
-            tableSize = findNextPrime(tableSize);
+            tableSize = numOfItems % load;
+            tableSize = nextPrime(tableSize);
 
             String[] hashTable = new String[tableSize];
             OAL oalLinear = new OAL(hashTable);
@@ -38,7 +42,7 @@ public class Driver {
             for (int i = 0; i < numOfItems; i++) {
                 //determine hashcode
                 hashCode = dictionary[i].hashCode();
-                hashCode = Math.abs(hashCode % tableSize);
+                hashCode = Math.abs(hashCode) % tableSize;
 
                 //Linear insert
                 oalLinear.linearInsert(hashCode, dictionary[i]);
@@ -46,11 +50,38 @@ public class Driver {
                 //Quadratic insert
                 oalQuadratic.quadraticInsert(hashCode, dictionary[i]);
             }
+
+            itemsWOCollisions = oalLinear.numOfItemsWOCollisions + oalQuadratic.numOfItemsWOCollisions;
+            totalCollisions = oalLinear.numOfCollisions + oalQuadratic.numOfCollisions;
+            System.out.println("Load value: " + load);
+            System.out.println("Items inserted without collisions: " + itemsWOCollisions);
+            System.out.println("Total number of Collisions: " + totalCollisions);
+            System.out.println();
         }
 
 
     }
-    public static int findNextPrime(int value) {
-        return 0;
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Method to find the next prime number after a given number
+    public static int nextPrime(int num) {
+        num++; // Start checking from the number after the input number
+        while (true) {
+            if (isPrime(num)) {
+                return num;
+            }
+            num++;
+        }
     }
 }
--- src/OAL.java
+++ src/OAL.java
@@ -4,21 +4,51 @@ linear and quadratic insert methods
  */
 public class OAL {
     String[] hashTable;
+    boolean noCollisions = true;
+    int numOfItemsWOCollisions = 0;
+    int numOfCollisions = 0;
+
     public OAL(String[] hashTable) {
         this.hashTable = hashTable;
     }
+
     public void linearInsert(int hashCode, String str) {
+        noCollisions = true;
         while (hashTable[hashCode] != null) {
+            noCollisions = false;
+            numOfCollisions++;
+
             hashCode++;
         }
+
         hashTable[hashCode] = str;
+
+        if (noCollisions) {
+            numOfItemsWOCollisions++;
+        }
     }
+
     public void quadraticInsert(int hashCode, String str) {
+        noCollisions = true;
         int value = 1;
         while (hashTable[hashCode] != null) {
+            noCollisions = false;
+            numOfCollisions++;
+
             hashCode = hashCode + (value * value);
             value++;
+
+            if (hashCode >= hashTable.length ||
+                hashCode < 0) {
+                hashCode = Math.abs(hashCode % hashTable.length);
+                System.out.println(hashCode);
+            }
         }
+
         hashTable[hashCode] = str;
+
+        if (noCollisions) {
+            numOfItemsWOCollisions++;
+        }
     }
 }
